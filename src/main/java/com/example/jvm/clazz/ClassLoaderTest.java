28d9bfa2bb7c1b3d52631f7d9b2f3fdee7ab58ff
liuji789 <583115762@qq.com>
2021-03-22 19:12:36 +0800

    双亲委派模型 findClass

--- src/main/java/com/example/jvm/clazz/ClassLoaderTest.java
+++ src/main/java/com/example/jvm/clazz/ClassLoaderTest.java
@@ -8,9 +8,22 @@ public class ClassLoaderTest {
             public Class<?> loadClass(String name) throws ClassNotFoundException {
                 return super.loadClass(name);
             }
+
+            /**
+             * 双亲委派模型，你需要告诉怎么find你需要的class
+             * @param name
+             * @return
+             * @throws ClassNotFoundException
+             */
+            @Override
+            protected Class<?> findClass(String name) throws ClassNotFoundException {
+                return super.findClass(name);
+            }
         };
         Object loadClass = classLoader.loadClass("com.example.jvm.clazz.ClassLoaderTest");
         System.out.println("loadClass.getClass() = " + loadClass.getClass());
         System.out.println("loadClass instanceof ClassLoaderTest = " + (loadClass instanceof ClassLoaderTest));
     }
+
+
 }
