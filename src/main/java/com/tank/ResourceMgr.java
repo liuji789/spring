d6b339457a65ba6e3352650d447dc0c4c81be908
liuji789 <583115762@qq.com>
2021-05-04 14:21:19 +0800

    Mac && Windows

--- src/main/java/com/tank/ResourceMgr.java
+++ src/main/java/com/tank/ResourceMgr.java
@@ -3,6 +3,7 @@ package com.tank;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -18,19 +19,19 @@ public class ResourceMgr {
 
     static {
         try {
-            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\tankL.gif"));
-            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\tankR.gif"));
-            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\tankU.gif"));
-            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\tankD.gif"));
+            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+File.separator+"tankL.gif"));
+            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"tankR.gif"));
+            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"tankU.gif"));
+            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"tankD.gif"));
 
 
-            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\bulletL.gif"));
-            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\bulletR.gif"));
-            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\bulletU.gif"));
-            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\bulletD.gif"));
+            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"bulletL.gif"));
+            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"bulletR.gif"));
+            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"bulletU.gif"));
+            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"bulletD.gif"));
 
             for (int i = 0; i < explodes.length; i++) {
-                explodes[i] = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static\\images\\e" + (i + 1) + ".gif"));
+                explodes[i] = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("static"+ File.separator+"images"+ File.separator+"e" + (i + 1) + ".gif"));
             }
 
         } catch (IOException e) {
