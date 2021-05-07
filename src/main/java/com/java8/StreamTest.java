f53d4f899d0f4c1fd83852310f7e43c433c1a79a
liuji789 <583115762@qq.com>
2021-05-07 19:19:10 +0800

    Stream

--- src/main/java/com/java8/StreamTest.java
+++ src/main/java/com/java8/StreamTest.java
@@ -26,12 +26,13 @@ public class StreamTest {
      */
     @Test
     public void test001(){
+        //通过集合
         List<Employee> emplees = EmployeeData.getEmplees();
         Stream<Employee> stream = emplees.stream();
         System.out.println("stream = " + stream);
         Stream<Employee> parallelStream = emplees.parallelStream();
         System.out.println("parallelStream = " + parallelStream);
-
+        //通过数组
         int[] ints = {1, 2, 3};
         IntStream stream1 = Arrays.stream(ints);
         System.out.println("stream1 = " + stream1);
@@ -39,18 +40,37 @@ public class StreamTest {
         Employee[] employees = {new Employee(1,"name1",1,1),new Employee(2,"name2",2,2)};
         Stream<Employee> stream2 = Arrays.stream(employees);
         System.out.println("stream2 = " + stream2);
-
+        //Stream.of
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
         System.out.println("integerStream = " + integerStream);
-
+        //创建无限流
+        //迭代
         Stream.iterate(0,t ->t+2).limit(10).forEach(System.out::println);
-
+        //生成
         Stream.generate(Math::random).limit(10).forEach(System.out::println);
     }
 
+    /**
+     * 中间操作：筛选与切片
+     * filter 接收lambda，从流中排除某些元素。
+     * limit 截断流，使元素不超过给定数量。
+     * skip 跳过元素，返回一个扔掉了前n个元素的流
+     * distinct 筛选，通过流的所生成元素的hashCode和equals去除重复元素。
+     */
     @Test
     public void test002(){
+        //查询薪水大于100的Employee
+        EmployeeData.getEmplees().stream().filter(employee -> employee.getSalary() > 100).forEach(System.out::println);
+
+    }
+
+    /**
+     * 映射
+     */
+    @Test
+    public void test003(){
         List<String> list = Arrays.asList("aa","bb","cc");
+        //接收一个函数作为参数，
         Stream<String> stream = list.stream().map(String::toUpperCase);
         stream.forEach(System.out::println);
 
@@ -62,8 +82,17 @@ public class StreamTest {
 
     }
 
+    public static Stream<Character> stringToStream(String s){
+        List<Character> characterList = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            characterList.add(c);
+        }
+
+        return characterList.stream();
+    }
+
     @Test
-    public void test003(){
+    public void test999(){
         List list = new ArrayList();
         list.add(1);
         list.add(2);
