d23706bb606a506968a0f91bc0c51f30fd582941
liuji789 <583115762@qq.com>
2021-05-12 23:59:12 +0800

    elasticsearch

---
+++ src/main/java/com/es/index/SearchIndex.java
@@ -0,0 +1,29 @@
+package com.es.index;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+public class SearchIndex {
+    public static void main(String[] args) throws IOException {
+        // 创建客户端对象
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        // 查询索引 - 请求对象
+        GetIndexRequest request = new GetIndexRequest("user");
+        // 发送请求，获取响应
+        GetIndexResponse response = client.indices().get(request, RequestOptions.DEFAULT);
+
+        System.out.println("aliases:"+response.getAliases());
+        System.out.println("mappings:"+response.getMappings());
+        System.out.println("settings:"+response.getSettings());
+
+        client.close();
+    }
+}
