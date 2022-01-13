f2ad24375d4b250bf2117d169d7cfe18f9138690
liuji789 <583115762@qq.com>
2022-01-13 20:22:02 +0800

    ~~~~ 2

---
+++ src/main/java/com/example/al2/class07/Inner.java
@@ -0,0 +1,9 @@
+package com.example.al2.class07;
+
+public class Inner<T> {
+	public T value;
+
+	public Inner(T v) {
+		value = v;
+	}
+}
---
+++ src/main/java/com/example/al2/class17/Graph.java
@@ -0,0 +1,14 @@
+package com.example.al2.class17;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Graph {
+	public HashMap<Integer, Node> nodes;
+	public HashSet<Edge> edges;
+	
+	public Graph() {
+		nodes = new HashMap<>();
+		edges = new HashSet<>();
+	}
+}
